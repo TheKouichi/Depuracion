dbbc91534b15606d2a58c93889e6e0dadd13046a
Ivan Amor Martin <elruso171@gmail.com>
2020-02-04 20:12:30 +0100

    Ejercicio de Depuracion con Eclipse completado

---
+++ EjercicioDepuracion.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class EjercicioDepuracion {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		int numeroLeido;
+		
+		for(int i = 0; i < 5; i++) {
+			
+			System.out.println(("Introduce un numero: "));
+			numeroLeido = input.nextInt();
+			
+			if(esPar(numeroLeido)) {
+				System.out.println("El numero leido es par: " + numeroLeido);
+			}
+			else {
+				System.out.println("El numero leido es impar: " + numeroLeido);
+			}
+		}
+		
+		System.out.println("El programa ha terminado");
+		
+		input.close();
+		
+
+	}
+	
+	static boolean esPar(int numero) {
+		System.out.println("Voy a evaluar un numero: ");
+		if(numero % 2 == 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
